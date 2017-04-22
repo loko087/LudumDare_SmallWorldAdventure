6a0598b9c9d311557988b7a5cc368eedc2d4327e
Jose Monge <jmonge@headlesschickengames.com>
2017-04-22 00:24:07 -0600

    modified scene, added TODO, added pointAndClick behavior

--- Assets/CharacterPointAndClickBehavior.cs
+++ Assets/CharacterPointAndClickBehavior.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityStandardAssets.Characters.ThirdPerson;
 public class CharacterPointAndClickBehavior : MonoBehaviour {
 
+    public GameObject bigCharacter;
+    public ThirdPersonCharacter thirdPersonController;
+    
 	// Use this for initialization
 	void Start () {
-		
+        thirdPersonController = bigCharacter.GetComponent<ThirdPersonCharacter>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        	
+        
 	}
+
+    
 }
